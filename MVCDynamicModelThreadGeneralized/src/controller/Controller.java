b98d9d02267bc13b491f0142648b830dc38d3ca9
Ronald van Dijk <r.van.dijk@pl.hanze.nl>
2017-02-01 09:59:29 +0100

    added javadoc

--- MVCDynamicModelThreadGeneralized/src/controller/Controller.java
+++ MVCDynamicModelThreadGeneralized/src/controller/Controller.java
@@ -4,12 +4,30 @@ import model.*;
 
 import java.awt.event.*;
 
+/**
+ * The controller reacts on input received from the view. 
+ * Here implementation is provided for the actionPerformed method
+ * that is implemented in the AbstractController.
+ * @author ronaldvandijk
+ * @version 01-02-2017
+ */
 public class Controller extends AbstractController {
 	
+	/**
+	 * Constructor for the controller.
+	 * @param model the model
+	 */
 	public Controller(Model model) {
 		super(model);		
 	}
 	
+	
+	/**
+	 * Implementation for the actionPerformed method that
+	 * this class has to provide. 
+	 * 
+	 * @param e the actionEvent that is given to the controller.
+	 */
 	public void actionPerformed(ActionEvent e) {
 		
 		
